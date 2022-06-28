f6580a2dd772b5b7005fcbb0961f823d4ba32594
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-06-28 23:14:22 +0500

    shurt circuit, form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import './App.css';
-<<<<<<< HEAD
-// import MultipleRendering from "./components/conditional-rendering/MultipleRendering";
-import UseEffectFetchData from './components/useEffect/UseEffectFetchData';
-
+import './App.css'
+import ShortCircuit from './components/short-circuit/ShortCircuit';
+import ControllerInput from './components/controller-input/ControllerInput';
 function App() {
   return (
     <div className="App">
-      <UseEffectFetchData />
-=======
-
-// import UseStateAdvansed from './components/useStateAdvansed/UseStateAdvansed';
-// import UseStateObject from './components/useStateAdvansed/UseStateObject';
-// import UseEffectBesics from './components/useEffect/UseEffectBesics';
-// import UseEffectCleanup from './components/useEffect/UseEffectCleanup';
-import MultipleRendering from './components/conditional-rendering/MultipleRendering';
-function App() {
-  return (
-    <div className="App">
-      <MultipleRendering />
-   
-
->>>>>>> 1088700d2152dbd680b426b09f37a44758e27df9
+      <ShortCircuit />
+      <ControllerInput/>
     </div>
   );
 }
