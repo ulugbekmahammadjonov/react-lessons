fcacf63d94a37e72e8a6b08e50f55b9388653915
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-06-28 07:02:39 +0500

    isLoading error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 // import UseStateAdvansed from './components/useStateAdvansed/UseStateAdvansed';
 // import UseStateObject from './components/useStateAdvansed/UseStateObject';
-import UseEffectBesics from './components/useEffect/UseEffectBesics';
-import UseEffectCleanup from './components/useEffect/UseEffectCleanup';
+// import UseEffectBesics from './components/useEffect/UseEffectBesics';
+// import UseEffectCleanup from './components/useEffect/UseEffectCleanup';
+import MultipleRendering from './components/conditional-rendering/MultipleRendering';
 function App() {
   return (
     <div className="App">
-      <UseEffectBesics />
-      <UseEffectCleanup />
+      <MultipleRendering />
+   
     </div>
   );
 }
