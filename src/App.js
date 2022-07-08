54ef3ecb949925a42229fab3ba0e95dbc0e5cbd5
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-07-08 08:52:02 +0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-<<<<<<< HEAD
-// import MultipleRendering from "./components/conditional-rendering/MultipleRendering";
-import UseEffectFetchData from './components/useEffect/UseEffectFetchData';
+import UseRef from './components/useRef/UseRef';
+
 
 function App() {
   return (
     <div className="App">
-      <UseEffectFetchData />
-=======
-
-// import UseStateAdvansed from './components/useStateAdvansed/UseStateAdvansed';
-// import UseStateObject from './components/useStateAdvansed/UseStateObject';
-// import UseEffectBesics from './components/useEffect/UseEffectBesics';
-// import UseEffectCleanup from './components/useEffect/UseEffectCleanup';
-import MultipleRendering from './components/conditional-rendering/MultipleRendering';
-function App() {
-  return (
-    <div className="App">
-      <MultipleRendering />
-   
-
->>>>>>> 1088700d2152dbd680b426b09f37a44758e27df9
+      <UseRef />
     </div>
   );
 }
