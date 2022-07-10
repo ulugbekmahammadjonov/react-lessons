80bea22064ce8a566fd2cb39e0db997a53f42a34
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-07-10 11:19:47 +0500

    Hooks: useReducer2

--- src/components/useReducer/Modal.jsx
+++ src/components/useReducer/Modal.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-const Modal = ({modalContent}) => {
+const Modal = ({modalContent, closeModal}) => {
+    useEffect(()=>{
+        setTimeout(()=>{
+            closeModal()
+        }, 1000)
+    })
   return (
     <div>{modalContent}</div>
   )
--- src/components/useReducer/UseReducer.jsx
+++ src/components/useReducer/UseReducer.jsx
@@ -1,30 +1,8 @@
 import React, { useState } from 'react'
 import { useReducer } from 'react'
 import Modal from './Modal';
+import{ reducer} from './reducer';
 
-
-const reducer = (state, action) => {
-    if(action.type === "ADD_ITEM"){
-            const newPeople = [...state.people, action.payload];
-           return {
-             ...state,
-             people: newPeople,
-             isModalOpen: true,
-             modalContent: "item added",
-           };
-  
-    }
-    if(action.type === "NO_VALUE"){
-        return{
-            ...state,
-            isModalOpen : true,
-            modalContent: "Please, Enter VAlue"
-        }
-    }
-      throw new Error("now matching action type")    
-    
-    
-};
 
 const defaultState = {
     people: [],
@@ -37,17 +15,20 @@ const UseReducer = () => {
    const handleSubmit = e =>{
     e.preventDefault()
       if (name) {
-        const newItem = { id: new Date().getTime.toString(), name };
+        const newItem = { id: new Date().getTime().toString(), name };
         dispatch({ type: "ADD_ITEM", payload: newItem });
+        setName("")
       }else{
         dispatch({type: "NO_VALUE"})
       }
    }
-
+   const closeModal = ()=>{
+    dispatch({type: "CLOSE_MODAL"})
+   }
 
   return (
     <div>
-    {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+    {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent}/>}
         <form className='form' onSubmit={handleSubmit}>
             <div>
                 <input type="text"
@@ -58,8 +39,9 @@ const UseReducer = () => {
         </form>
         {state.people.map((person)=>{
             return(
-                <div key={person.id}>
+                <div className='item' key={person.id}>
                     <h4>{person.name}</h4>
+                    <button onClick={()=>dispatch({type : "REMOVE_ITEM", pyload: person.id})}>remove</button>
                 </div>
             )
         })}
