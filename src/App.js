152cbdf046921505e64c91d8b73e165a0a49739d
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-07-10 10:20:39 +0500

    hooks: useReduser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
-import UseRef from './components/useref-basics/UseRef';
+import UseReducer from './components/useReducer/UseReducer';
+
 
 function App() {
   return (
     <div className="App">
-      <UseRef/>
+      <UseReducer />
     </div>
   );
 }
---
+++ src/components/useReducer/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Modal = ({modalContent}) => {
+  return (
+    <div>{modalContent}</div>
+  )
+}
+
+export default Modal;
